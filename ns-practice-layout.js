6639ff9128307128b7024b82efdeaa1a77f28a4c
saus883 <ganeshtrooper89@gmail.com>
2026-03-19 09:21:30 -0500

    Delete ns-practice-layout.js

--- ns-practice-layout.js
+++
@@ -1,9 +0,0 @@
-function togglePage(pageToShowClass) {
-  const pageToShow = document.querySelector(`.${pageToShowClass}`);
-  const allPages = document.querySelectorAll('.page');
-
-  for (let i = 0; i < allPages.length; i++) {
-    allPages[i].classList.add('hidden');
-  }
-  pageToShow.classList.remove('hidden');
-}
